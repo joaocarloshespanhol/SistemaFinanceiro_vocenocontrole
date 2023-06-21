3b91de947557604a0f48429ad043d3b27948dcbe
joaocarloshespanhol <joaocarlos.hespanhol@hotmail.com>
2023-06-21 18:35:07 -0300

    create grafics

---
+++ src/components/financas/BarChartjs.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import { Bar } from "react-chartjs-2";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+const BarChart = () => {
+    const [chartData, setChartData] = useState ({
+        datasets: [],
+
+    });
+
+const [chartOptions, setChartOptions] = useState({});
+
+useEffect(() => {
+    setChartData({
+        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        datasets: [
+            {
+                label: 'Receitas',
+                data: [1820, 2201, 19490, 17938, 24182, 17842, 22475],
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgb(53, 162, 235, 10',
+                borderRadius: 2
+            },
+        ]
+    })
+}, [])
+
+    return (
+        <>
+            <div className="w-full">
+                <Bar data={chartData} options={chartOptions} />
+            </div>
+        </>
+    );
+};
+
+export default BarChart;
--- src/components/financas/Graficos.tsx
+++ src/components/financas/Graficos.tsx
@@ -1,13 +1,15 @@
+import BarChart from "./BarChartjs";
+import BarChart2 from "./BarChartjs";
 
 export default function Graficos() {
     return(
         <div className="mx-auto">
             <div className="flex xl:flex-row flex-col gap-16 xl:gap-8">
                 <div className="flex box-shadow w-80 h-72 md:w-[710px] md:h-[420px] lg:w-[960px] lg:h-[420px] xl:w-[620px] xl:h-[490px] 2xl:w-[910px] 2xl:h-[] rounded-lg border border-zinc-800">
-                    
+                    <BarChart />
                 </div>
                 <div className="flex box-shadow w-80 h-72 md:w-[710px] md:h-[420px] lg:w-[960px] lg:h-[420px] xl:w-[620px] xl:h-[490px] 2xl:w-[910px] 2xl:h-[] rounded-lg border border-zinc-800">
-                    
+                    <BarChart2 />
                     </div>
             </div>
             Gráfico em breve.....
