67449cd7eda3f351ea84cd1b6d93e8c0123ec082
joaocarloshespanhol <joaocarlos.hespanhol@hotmail.com>
2023-06-21 17:51:32 -0300

    feat: grafico

--- src/components/financas/Graficos.tsx
+++ src/components/financas/Graficos.tsx
@@ -2,11 +2,11 @@
 export default function Graficos() {
     return(
         <div className="mx-auto">
-            <div className="flex flex-col gap-16">
-                <div className="flex box-shadow w-80 h-72 md:w-[710px] md:h-[420px] lg:w-[960px] lg:h-[420px] xl:w-[1220px] xl:h-[490px] rounded-lg border border-zinc-800">
+            <div className="flex xl:flex-row flex-col gap-16 xl:gap-8">
+                <div className="flex box-shadow w-80 h-72 md:w-[710px] md:h-[420px] lg:w-[960px] lg:h-[420px] xl:w-[620px] xl:h-[490px] 2xl:w-[910px] 2xl:h-[] rounded-lg border border-zinc-800">
                     
                 </div>
-                <div className="flex box-shadow w-80 h-72 d:w-[710px] md:h-[420px] lg:w-[960px] lg:h-[420px] xl:w-[1220px] xl:h-[690px] rounded-lg border border-zinc-800">
+                <div className="flex box-shadow w-80 h-72 md:w-[710px] md:h-[420px] lg:w-[960px] lg:h-[420px] xl:w-[620px] xl:h-[490px] 2xl:w-[910px] 2xl:h-[] rounded-lg border border-zinc-800">
                     
                     </div>
             </div>
