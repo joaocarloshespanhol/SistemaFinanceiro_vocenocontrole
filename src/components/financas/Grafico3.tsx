2e16b68ab89c6455ab361541197fd00dd12c3dd7
joaocarloshespanhol <joaocarlos.hespanhol@hotmail.com>
2023-07-04 18:28:08 -0300

    att

--- src/components/financas/Grafico3.tsx
+++ src/components/financas/Grafico3.tsx
@@ -36,7 +36,7 @@ export const options = {
   },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const labels = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
 
 export const data = {
   labels,
@@ -44,14 +44,14 @@ export const data = {
     {
       fill: true,
       label: 'Receitas',
-      data: [1400, 1680, 1800, 1980, 2500, 2650, 3150, 3800],
+      data: [1400, 1680, 1800, 1980, 2500, 2650, 3150, 3800, 3900, 4100, 4350, 4400, 5600],
       borderColor: 'rgb(53, 162, 235)',
       backgroundColor: 'rgba(53, 162, 235, 0.1)',
     },
     {
       fill: true,
       label: 'Despesas',
-      data: [400, 600, 900, 1200, 1400, 1500, 1900, 2000],
+      data: [400, 600, 900, 1200, 1400, 1500, 1900, 2000, 1800, 1400, 1600, 1500, 1300, 1900],
       borderColor: 'rgb(114, 15, 114)',
       backgroundColor: 'rgb(119, 15, 104, 0.1)',
     },
