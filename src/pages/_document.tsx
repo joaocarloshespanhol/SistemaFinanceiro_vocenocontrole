5538f91e52c8d3d57f044a3cc1cd4daedbf19117
joaocarloshespanhol <joaocarlos.hespanhol@hotmail.com>
2023-07-21 00:39:12 -0300

    head-title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" className='scroll-smooth'>
-      <Head />
+      <Head title="Você no Controle Financeiro"/>
       <body>
         <Main />
         <NextScript />
