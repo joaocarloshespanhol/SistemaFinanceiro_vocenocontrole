4d0bb10c76c14390f15578f219ea5c79221ff759
joaocarloshespanhol <joaocarlos.hespanhol@hotmail.com>
2023-10-20 14:35:46 -0300

    att logo.svg

--- src/components/landing/comum/Logo.tsx
+++ src/components/landing/comum/Logo.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Logo() {
     return (
-        <div className="mb-4">
-            <Image src="/Logo.png" width={120} height={120} alt="logo" />
+        <div className="">
+            <Image src="/Logo.svg" width={120} height={120} alt="logo" />
         </div>
     );
 }
